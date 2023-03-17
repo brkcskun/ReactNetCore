22bf41689278dbd10b0dfc58f859885b4458d412
Burak Coşkun <burakc34@gmail.com>
2023-03-18 01:19:05 +0300

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=.;Database=myDataBase;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
